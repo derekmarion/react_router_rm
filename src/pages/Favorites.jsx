37b44b02cf8bb2f022914828f6d88be1353e62dd
Derek Marion <derek.j.marion@gmail.com>
2023-11-07 12:34:24 -0800

    Add ability to remove favorites on Favorites page

---
+++ src/pages/Favorites.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState} from "react";
+import Card from 'react-bootstrap/Card';
+import { useOutletContext } from "react-router-dom";
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import ListGroup from 'react-bootstrap/ListGroup';
+import Button from 'react-bootstrap/Button'
+
+const Favorites = () => {
+    const { favorites, setFavorites } = useOutletContext();
+
+    const removeFromFavorites = (id) => {
+        // Filter out items with the specified id
+        const updateFavorites = favorites.filter(favorite => favorite.id !== id);
+        setFavorites(updateFavorites);
+    }
+
+    return (
+        <>
+        <div className="container text-center" style={{ marginTop: '70px' }}>
+        <h1>Favorites</h1>
+        <Row xs={1} md={2} lg={3} xl={4} className="g-4">
+        {favorites.map(favorite => (
+            <Col key={favorite.id}>
+            <Card style={{ width: '18rem'}}>
+                <Card.Img variant="top" src={favorite.image} />
+                <Card.Body>
+                    <Card.Title>{favorite.name}</Card.Title>
+                    <ListGroup variant="flush">
+                        <ListGroup.Item>Status: {favorite.status}</ListGroup.Item>
+                        <ListGroup.Item>Species: {favorite.species}</ListGroup.Item>
+                        <ListGroup.Item><Button onClick={()=> removeFromFavorites(favorite.id)}>Remove</Button></ListGroup.Item>
+                    </ListGroup>
+                </Card.Body>
+            </Card>
+            </Col>
+        ))}
+        </Row>
+        </div>
+        </>
+    )
+        }
+
+export default Favorites;
