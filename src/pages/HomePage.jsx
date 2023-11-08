8a23c03f8e092b8069e8ace970a1f0668291cc1c
Derek Marion <derek.j.marion@gmail.com>
2023-11-08 14:08:59 -0800

    Add carousel to homepage with full width images

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,34 @@
 import React from "react";
-import { logo } from '../images/index.js';
+import { rm1, rm2, rm3 } from '../images/index.js';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Carousel } from "react-bootstrap";
 
 function HomePage() {
     return (
         <>
-        <div className="container text-center" style={{ marginTop: '70px' }}>
-        <img src={logo} alt="Rick and Morty Logo" className="mx-auto"/>
-        </div>
+        <Carousel className="w-100" style={{ marginTop: '50px' }}>
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src={rm1}
+            alt="First slide"
+            />
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src={rm2}
+            alt="First slide"
+            />
+        </Carousel.Item>
+        <Carousel.Item>
+            <img
+            className="d-block w-100"
+            src={rm3}
+            alt="First slide"
+            />
+        </Carousel.Item>
+        </Carousel>
         </>
     )
 }
